7d97c90a6d3af4227e1f36ac6e7c4f3a66178b5c
makeforfun <171019309+makeforfun@users.noreply.github.com>
2026-03-19 16:09:47 +0000

    Add files via upload

---
+++ Econ Netvert/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Econ Netvert")> 
+<Assembly: AssemblyDescription("c# <-> vb.net code/projects converter")> 
+<Assembly: AssemblyCompany("CocoaCupMorning")> 
+<Assembly: AssemblyProduct("Econ Netvert")> 
+<Assembly: AssemblyCopyright("Copyright ©  2026")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(True)> 
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("ad15c90b-bc0d-472e-9d94-f87cea53b57a")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("2.4.5.19")> 
+<Assembly: AssemblyFileVersion("2.4.5.19")> 
---
+++ README.md
@@ -0,0 +1,6 @@
+# .net 4 code converter software
+Requires .net 4.0
+
+Author http://github.com/otherdeniz/EconNetvert
+
+Rework http://github.com/makeforfun/EconNetvert
